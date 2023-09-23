dc673db35defd15a208bd1cf302457d03bb8fd72
pravin1desh@gmail.com <1521629029Pp@gh>
2023-09-23 13:49:28 +0530

    tawk changes

--- pages/_app.js
+++ pages/_app.js
@@ -15,18 +15,18 @@ function MyApp({ Component, pageProps }) {
   };
 
   useEffect(() => {
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    Tawk_API.embedded = "tawk_5fc258c9920fc91564cb894d";
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = "https://embed.tawk.to/5fc258c9920fc91564cb894d/1estddag9";
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
+    <Script id="tawk" strategy="lazyOnload">
+      {`
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_5fc258c9920fc91564cb894d';
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5fc258c9920fc91564cb894d/1estddag9';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);})();
+    `}
+    </Script>;
 
     setLoading(true);
     // setTimeout(() => {
