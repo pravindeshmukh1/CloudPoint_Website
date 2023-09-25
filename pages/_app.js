50922c304075d8f2cc58a55fc6d2b2bc6f2a7dcd
pravin1desh@gmail.com <1521629029Pp@gh>
2023-09-25 10:00:51 +0530

    refc : tawk code

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import "../public/assets/css/tailwind-built.css";
 import Preloader from "../components/elements/Preloader";
 import "swiper/css";
 import Script from "next/script";
-// import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 // import dynamic from "next/dynamic";
 
 // const TawkMessengerReact = dynamic(
@@ -25,18 +25,18 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // initialRenderRef.current = true;
 
-    var Tawk_API = Tawk_API || {},
-    Tawk_LoadStart = new Date();
-  Tawk_API.embedded = "tawk_5fc258c9920fc91564cb894d";
-  (function () {
-    var s1 = document.createElement("script"),
-      s0 = document.getElementsByTagName("script")[0];     
-    s1.async = true;
-    s1.src = "https://embed.tawk.to/5fc258c9920fc91564cb894d/1estddag9";
-    s1.charset = "UTF-8";
-    s1.setAttribute("crossorigin", "*");
-    s0.parentNode.insertBefore(s1, s0);
-  })();
+  //   var Tawk_API = Tawk_API || {},
+  //   Tawk_LoadStart = new Date();
+  // Tawk_API.embedded = "tawk_5fc258c9920fc91564cb894d";
+  // (function () {
+  //   var s1 = document.createElement("script"),
+  //     s0 = document.getElementsByTagName("script")[0];     
+  //   s1.async = true;
+  //   s1.src = "https://embed.tawk.to/5fc258c9920fc91564cb894d/1estddag9";
+  //   s1.charset = "UTF-8";
+  //   s1.setAttribute("crossorigin", "*");
+  //   s0.parentNode.insertBefore(s1, s0);
+  // })();
 
 
     setLoading(true);
@@ -104,12 +104,12 @@ function MyApp({ Component, pageProps }) {
         src="https://embed.tawk.to/5fc258c9920fc91564cb894d/1estddag9" async
       /> */}
       {/* <div id="tawk_5fc258c9920fc91564cb894d"></div> */}
-      {/* <TawkMessengerReact
+      <TawkMessengerReact
         propertyid="5fc258c9920fc91564cb894d"
         widgetid="1estddag9"
         onLoad={onLoad}
       /> 
- */}
+
       {/* {initialRenderRef.current && (
         <TawkMessengerReact
           propertyid="5fc258c9920fc91564cb894d"
